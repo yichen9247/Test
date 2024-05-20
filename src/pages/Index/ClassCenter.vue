4a9c61ad5ede5d294e154fd72edbc6ab45b26e65
yichen9247 <865252486@qq.com>
2024-05-20 17:45:59 +0800

    ADD MORE

--- src/pages/Index/ClassCenter.vue
+++ src/pages/Index/ClassCenter.vue
@@ -5,17 +5,35 @@
     import { VueDraggable } from 'vue-draggable-plus'
     const setInfoDialog = (pageId) => open(`../task?id=${pageId}`);
 
-    const list = ref([
-        '项目一：人机交互系统检修','项目二：全景影像系统检修','项目三：倒车辅助PAS系统检修','项目四：并线辅助BSM系统检修','项目五：集成式自适应巡航IACC系统检修'
+    const classList = ref([
+        {
+            id: 1,
+            name: '项目一：人机交互系统检修'
+        }
+        ,{
+            id: 2,
+            name: '项目二：全景影像系统检修'
+        },
+        {
+            id: 3,
+            name: '项目三：倒车辅助PAS系统检修'
+        },
+        {
+            id: 4,
+            name: '项目四：并线辅助BSM系统检修'
+        },
+        {
+            id: 5,
+            name: '项目五：集成式自适应巡航IACC系统检修'
+        }
     ]);
-    
 </script>
 
 <template>
     <div class="page-container">
         <ul class="class-list"> 
-            <VueDraggable v-model="list" class="drag-container" :animation="300">
-                <li v-for="item in list" :key="list.indexOf(item)" class="class-list-item" @click="setInfoDialog(list.indexOf(item)+1)">{{ list[list.indexOf(item)] }}</li>
+            <VueDraggable v-model="classList" class="drag-container" :animation="300">
+                <li v-for="item in classList" :key="classList.indexOf(item)" class="class-list-item" @click="setInfoDialog(classList[classList.indexOf(item)].id)">{{ classList[classList.indexOf(item)].name }}</li>
             </VueDraggable>
         </ul>
     </div>
